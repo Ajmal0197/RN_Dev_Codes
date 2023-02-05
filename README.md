93f782b4342f2bf08bc2873b4bb958f4fb7de252
Ajmal0197 <ajmal0197@gmail.com>
2023-02-05 22:57:32 +0530

    dynamic text input

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. React native redux toolkit with thunk, persist, reactotron.(srcReduxToolkit)
 2. App performance coding.(srcAppPerformance)
+3. React native dynamic multiple text input with add/remove functionality.
--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './srcReduxToolkit/App';
+// import App from './srcReduxToolkit/App';
+// import App from './srcDynamicTextInput';
+// import App from './srcAppPerformance/reactMemo';
+// import App from './srcAppPerformance/useCallback';
+import App from './srcAppPerformance/useMemo';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
